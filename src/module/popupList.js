344b8a99cebff1655bcc3f050309dec1ccd78b22
girma <girmatarekegn.gi@gmail.com>
2022-12-07 15:05:35 +0300

    Add updated index.html popuplist listoFitem

--- src/module/popupList.js
+++ src/module/popupList.js
@@ -1,3 +1,5 @@
+import { getComment, postComment } from './commentPostGet.js';
+
 const popupList = async (mealId) => {
   const popupContainer = document.querySelector('.home');
   const mealData = await fetch(`https:www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
@@ -5,6 +7,12 @@ const popupList = async (mealId) => {
   const data = mealJson.meals;
   const mealImg = data[0].strMealThumb;
   const name = data[0].strMeal;
+
+  const newData = await getComment(mealId);
+  let len = newData.length;
+  if (len === undefined) {
+    len = 0;
+  }
 
   const description = data[0].strInstructions;
   popupContainer.innerHTML = `
@@ -25,6 +33,15 @@ const popupList = async (mealId) => {
                 <div class="description">
                     <p>${description}</p>
                 </div>
+                <div class="count-com">
+                    <div class="num-com"><h2>Comment (<span id="_${mealId}">${len}</span>)</h2><div>
+                    <div class="add-comment">
+                        <div class="List-item">
+                            <ul class="add-comment-list">
+                            </ul>
+                    </div>
+                    </div>
+                </div>
                 <div class="form-new">
                     <div class="center-div">
                         <h2>Add comment</h2>
@@ -44,7 +61,32 @@ const popupList = async (mealId) => {
   const closeBtn = document.querySelector('.closeBtn');
   closeBtn.addEventListener('click', () => {
     popupContainer.classList.add('hide');
+    popupContainer.classList.remove('opacity-class');
   });
-};
 
+  const form = document.querySelector('.form-popup');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const user = document.getElementById('user').value;
+    const comment = document.getElementById('comment').value;
+
+    const commentLen = document.getElementById(`_${mealId}`);
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1; // months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const listAdd = document.querySelector('.add-comment-list');
+    listAdd.innerHTML += `
+    <li>${user}  |  <span>${comment}</span>  | <span>${year}-${month}-${day}</span></li>
+    `;
+    let currentComment = Number(commentLen.innerHTML);
+    currentComment += 1;
+    commentLen.innerHTML = currentComment;
+
+    form.reset();
+    postComment(mealId, user, comment);
+  });
+  getComment(mealId);
+};
 export default popupList;
